3a8f6baba3b898f84ed5ff674b8397f4b053d8a1
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-04-08 01:08:33 -0700

    added dependencies to index

---
+++ index.js
@@ -0,0 +1,6 @@
+//connecting to db importing connection.js
+const db = require("./db/connection");
+const mysql2 = require('mysql2');
+const inquirer = require('inquirer');
+const console_table = require('console.table');
+
