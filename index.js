8e62537be9bedf8dd40e5d879802ad9f05003e59
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-04-08 02:37:12 -0700

    once db has connected inquire.prompt doesnt wait for user response

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
 //connecting to db importing connection.js
 const db = require("./db/connection");
-const mysql2 = require('mysql2');
-const inquirer = require('inquirer');
-const console_table = require('console.table');
+const mysql2 = require("mysql2");
+const inquirer = require("inquirer");
+const console_table = require("console.table");
 
+
+async function runApp() {
+    
+    console.log(`
+  
+===========================================================================
+*************************** Work Force ************************************ 
+===========================================================================
+    `);
+   inquirer.prompt([
+      {
+        type: "list",
+        name: "tasks",
+        message: "What can Work Force help you with today?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee's role",
+        ],
+      },
+    ]);
+    
+}
+
+
+//     // .then((response) => {
+//     //   console.log(response);
+//     //   determineAction(response);
+//     // });
+// };
+
+const determineAction = (answer) =>  {
+    if (answer === "View all departments") {
+      console.log("display all department");
+    } else if (answer === "View all roles") {
+      console.log("display all roles");
+    } else if (answer === "View all employees") {
+      console.log("display all employees");
+    }
+}
+
+runApp();
